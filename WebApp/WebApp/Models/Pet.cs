a46d50efc5fb0166768b8c2ed9c73a376f0285ac
manglusia <stasiek.rupala@gmail.com>
2024-04-15 00:28:21 +0200

    aaaa

---
+++ WebApp/WebApp/Models/Pet.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp.Models;
+
+public class Pet
+{
+    public int IdPet { get; set; }
+    public string PetName { get; set; }
+    public string PetCategory { get; set; }
+    public double Weight { get; set; }
+    public string HairColor { get; set; }
+}
