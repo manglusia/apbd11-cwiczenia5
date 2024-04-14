7d7d0c155226018414fe968c507e6c69cc7f14f6
manglusia <stasiek.rupala@gmail.com>
2024-04-15 00:21:25 +0200

    Added Class Pet

--- WebApp/WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4b3b3e6ab6530fac0ed4f8688bde391ba287c0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
