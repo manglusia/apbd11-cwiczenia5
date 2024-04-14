5ae794233bd7164c64e05b5f1d7a3ab5afbbd9fc
manglusia <stasiek.rupala@gmail.com>
2024-04-15 00:46:31 +0200

    Visit Class Added

---
+++ WebApp/WebApp/Models/Visit.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp.Models;
+
+public class Visit
+{
+    public string VisitDate { get; set; }
+    public Pet VisPet { get; set; }
+    public string VisitDescription { get; set; }
+    public double VisitCost { get; set; }
+    
+}
