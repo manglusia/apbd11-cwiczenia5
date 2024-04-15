a82b43cf64ce245f7d8d06aa35d710c9f89461d7
manglusia <stasiek.rupala@gmail.com>
2024-04-15 23:52:02 +0200

    Task Complete

--- WebApp/WebApp/Controllers/VisitController.cs
+++ WebApp/WebApp/Controllers/VisitController.cs
@@ -12,13 +12,18 @@ public class VisitController : ControllerBase
     
     private static List<Visit> _visits = new()
     {
-        new Visit { VisitDate = "24-10-2003", VisPet = null, VisitCost = 100.0, VisitDescription = "Everthings good"}
+        new Visit { VisitDate = "24-10-2003", VisPet = new Pet{IdPet = 1, PetName = "Korgi", HairColor = "White", PetCategory = "Dog", Weight = 50.0}, VisitCost = 100.0, VisitDescription = "Everthings good"},
+        new Visit { VisitDate = "25-10-2003", VisPet = new Pet{IdPet = 2, PetName = "Kajtek", HairColor = "Brazowy", PetCategory = "Dog", Weight = 30.0}, VisitCost = 1000.0, VisitDescription = "Everthings bad"},
+        new Visit { VisitDate = "26-10-2003", VisPet = new Pet{IdPet = 3, PetName = "Wargas", HairColor = "Black", PetCategory = "Dog", Weight = 45.0}, VisitCost = 10.0, VisitDescription = "Everthings ok"},
+        new Visit { VisitDate = "27-10-2003", VisPet = new Pet{IdPet = 3, PetName = "Wargas", HairColor = "Black", PetCategory = "Dog", Weight = 45.0}, VisitCost = 500.0, VisitDescription = "Everthings BADD"}
+        
     };
     
     [HttpGet("{idVi:int}")]
     public IActionResult GetVisit(int idVi)
     {
-        return Ok(_visits);
+        var visit = _visits.FindAll(a => a.VisPet.IdPet == idVi);
+        return Ok(visit);
     }
     
     [HttpPost]
